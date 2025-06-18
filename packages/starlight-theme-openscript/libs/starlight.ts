4c45af716497551e18d142679b4d8f45d8419802
Robin <git+r@obin.ch>
2025-06-18 19:07:29 +0000

    chore: fix overrides

--- packages/starlight-theme-openscript/libs/starlight.ts
+++ packages/starlight-theme-openscript/libs/starlight.ts
@@ -9,15 +9,15 @@ export function overrideComponents(
   const components = { ...starlightConfig.components }
   for (const override of overrides) {
     if (starlightConfig.components?.[override]) {
-      const fallback = `starlight-theme-openscript/overrides/${override}.astro`
+      const fallback = `@openscript-ch/starlight-theme/overrides/${override}.astro`
 
       logger.warn(`A \`<${override}>\` component override is already defined in your Starlight configuration.`)
       logger.warn(
-        `To use \`starlight-theme-openscript\`, either remove this override or manually render the content from \`${fallback}\`.`,
+        `To use \`@openscript-ch/starlight-theme\`, either remove this override or manually render the content from \`${fallback}\`.`,
       )
       continue
     }
-    components[override] = `starlight-theme-openscript/overrides/${override}.astro`
+    components[override] = `@openscript-ch/starlight-theme/overrides/${override}.astro`
   }
 
   return components
